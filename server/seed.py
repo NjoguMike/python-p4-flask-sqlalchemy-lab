9c2e2a3b9ba0a3f1957d2f8a3b4c2e0985b0912b
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-11-19 15:54:18 +0300

    Complete flask Lab

--- server/seed.py
+++ server/seed.py
@@ -43,6 +43,8 @@ with app.app_context():
         a = Animal(name=name, species=rc(species))
         a.zookeeper = rc(zookeepers)
         a.enclosure = rc(enclosures)
+        # a.zookeper_id = a.zookeper.id
+        # a.enclosure_id = a.enclosure_id.id
         animals.append(a)
 
     db.session.add_all(animals)
